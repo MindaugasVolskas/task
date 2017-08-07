98b5f4319d0953da10c9bfd2001afa0633676434
Mindaugas Volskas <Mindaugas.Volskas@gmail.com>
2017-08-07 19:41:52 +0300

    setup

---
+++ src/main/java/com/mindaugas/metasite/MetasiteApplication.java
@@ -0,0 +1,12 @@
+package com.mindaugas.metasite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetasiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetasiteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mindaugas/metasite/MetasiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mindaugas.metasite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MetasiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
